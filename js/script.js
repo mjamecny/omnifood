2e93262bc98b2d994999ca04fb38f771a665bc47
roydett <94c884c8-c756-4c1a-a30d-d897c94d81b5@anonaddy.me>
2022-04-20 16:22:02 +0200

    implementing smooth scrolling

--- js/script.js
+++ js/script.js
@@ -6,22 +6,33 @@ btnNavEl.addEventListener('click', function(){
   headerEl.classList.toggle('nav-open')
 })
 
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll('a:link')
 
+allLinks.forEach(function(link){
+  link.addEventListener('click', function(e){
+    e.preventDefault()
+    const href = link.getAttribute('href')
+    // console.log(href)
 
+    // Scroll back to top
+    if(href === '#') window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
 
+    // Scroll to other links
+    if(href !== '#' && href.startsWith('#')){
+      const sectionEl = document.querySelector(href)
+      sectionEl.scrollIntoView({behavior: 'smooth'})
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
+    // Close mobile navigation
+    if(link.classList.contains('main-nav-link')){
+      headerEl.classList.toggle('nav-open')
+    }
+  })
+})
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
@@ -37,7 +48,7 @@ function checkFlexGap() {
   document.body.appendChild(flex);
   var isSupported = flex.scrollHeight === 1;
   flex.parentNode.removeChild(flex);
-  console.log(isSupported);
+  // console.log(isSupported);
 
   if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
