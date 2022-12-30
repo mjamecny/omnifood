4b86b1fb42ec99f9ce210512eebf5bc53a60b36b
roydett <94c884c8-c756-4c1a-a30d-d897c94d81b5@anonaddy.me>
2022-12-30 12:31:55 +0100

    last commit

--- js/script.js
+++ js/script.js
@@ -2,33 +2,34 @@
 const btnNavEl = document.querySelector('.btn-mobile-nav')
 const headerEl = document.querySelector('.header')
 
-btnNavEl.addEventListener('click', function(){
+btnNavEl.addEventListener('click', function () {
   headerEl.classList.toggle('nav-open')
 })
 
 //////////////////////// Smooth scrolling animation
 const allLinks = document.querySelectorAll('a:link')
 
-allLinks.forEach(function(link){
-  link.addEventListener('click', function(e){
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
     e.preventDefault()
     const href = link.getAttribute('href')
     // console.log(href)
 
     // Scroll back to top
-    if(href === '#') window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    })
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      })
 
     // Scroll to other links
-    if(href !== '#' && href.startsWith('#')){
+    if (href !== '#' && href.startsWith('#')) {
       const sectionEl = document.querySelector(href)
-      sectionEl.scrollIntoView({behavior: 'smooth'})
+      sectionEl.scrollIntoView({ behavior: 'smooth' })
     }
 
     // Close mobile navigation
-    if(link.classList.contains('main-nav-link')){
+    if (link.classList.contains('main-nav-link')) {
       headerEl.classList.toggle('nav-open')
     }
   })
@@ -36,41 +37,45 @@ allLinks.forEach(function(link){
 
 //////////////////////// Sticky navigation
 const sectionHeroEl = document.querySelector('.section-hero')
-const obs = new IntersectionObserver(function(entries){
-  const ent = entries[0]
-  console.log(ent)
-  if(!ent.isIntersecting){
-    document.body.classList.add('sticky')
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0]
+    console.log(ent)
+    if (!ent.isIntersecting) {
+      document.body.classList.add('sticky')
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove('sticky')
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px',
   }
-  if(ent.isIntersecting) {
-    document.body.classList.remove('sticky')
-  }
-}, {
-  // In the viewport
-  root: null,
-  threshold: 0,
-  rootMargin: '-80px'
-})
+)
 obs.observe(sectionHeroEl)
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
-  var flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+  var flex = document.createElement('div')
+  flex.style.display = 'flex'
+  flex.style.flexDirection = 'column'
+  flex.style.rowGap = '1px'
 
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement('div'))
+  flex.appendChild(document.createElement('div'))
 
-  document.body.appendChild(flex);
-  var isSupported = flex.scrollHeight === 1;
-  flex.parentNode.removeChild(flex);
+  document.body.appendChild(flex)
+  var isSupported = flex.scrollHeight === 1
+  flex.parentNode.removeChild(flex)
   // console.log(isSupported);
 
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+  if (!isSupported) document.body.classList.add('no-flexbox-gap')
 }
-checkFlexGap();
+checkFlexGap()
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
